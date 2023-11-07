f8a769fd11c99b58c70113a0cf601514dd7eac98
tanuj96 <tanujmittal96@gmail.com>
2023-11-08 00:55:47 +0530

    episode 3 React Functional Components , Component Composition

--- App.js
+++ App.js
@@ -13,3 +13,24 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(jsxHeading);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+);
+
+//React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
