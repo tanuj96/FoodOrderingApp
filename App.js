562e62b699e6d10a7da51c10a7e20142a811c5c6
tanuj96 <tanujmittal96@gmail.com>
2023-11-08 01:14:49 +0530

    episode 3

--- App.js
+++ App.js
@@ -14,19 +14,56 @@
 
 // root.render(jsxHeading);
 
+// -------------------------------------------------------------------------------------------------------------------
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// const Title = () => (
+//   <h1 className="head" tabIndex="5">
+//     Namaste React using JSX
+//   </h1>
+// );
+
+// //React Functional Component
+// const HeadingComponent = () => (
+//   <div id="container">
+//     <Title />
+//     <h1 className="heading">Namaste React Functional Component</h1>
+//   </div>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<HeadingComponent />);
+
+// -------------------------------------------------------------------------------------------------------------------
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const elem = <span>React Element</span>;
+
 const Title = () => (
   <h1 className="head" tabIndex="5">
+    {elem}
     Namaste React using JSX
   </h1>
 );
 
-//React Functional Component
+const Title2 = (
+    <h1 className="head" tabIndex="5">
+      {elem}
+      Namaste React using JSX
+    </h1>
+  );
+
 const HeadingComponent = () => (
   <div id="container">
+    {Title()}
     <Title />
+    <Title></Title>
+    {Title2}
     <h1 className="heading">Namaste React Functional Component</h1>
   </div>
 );
