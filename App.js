44275c66bbf726399b0576e02a5048c84ff90d40
tanuj96 <tanujmittal96@gmail.com>
2023-11-08 04:10:30 +0530

    Body , Food Card Component

--- App.js
+++ App.js
@@ -9,17 +9,23 @@ import ReactDOM from "react-dom/client";
  * -Search
  * -RestraurantContainer
  *    -RestautrantCard
+ *      -Img
+ *      -Name of Res, Star Rating, Cuisine, Delivery Time
  * Footer
  * -Copyright
  * -Links
  * -Address
  * -Contact
  */
-const Header = () =>{
+
+const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/25539c29532269.55f7d6a0a8c71.jpg" />
+        <img
+          className="logo"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/25539c29532269.55f7d6a0a8c71.jpg"
+        />
       </div>
       <div className="nav-items">
         <ul>
@@ -30,14 +36,61 @@ const Header = () =>{
         </ul>
       </div>
     </div>
-  )
-}
-const AppLayout = () =>{
+  );
+};
+
+const RestaurantCard = () => {
   return (
-  <div className="app">
-    <Header/>
-  </div>)
-}
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="food-image"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO9zSzY4NfU2UYkx3S51gFiUuHSJjK5X_97w&usqp=CAU"
+      />
+      <h3>Meghna Foods</h3>
+      <h4 >North Indian, South Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
