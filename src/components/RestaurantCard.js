d2ff392ae6a24d4faa317c6686dde11e56d5935d
tanuj96 <tanujmittal96@gmail.com>
2023-11-16 18:37:01 +0530

    costForTwo parameter fix in Restro Card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -24,7 +24,7 @@ const RestaurantCard = (props) => {
         <h3>{name}</h3>
         <h4>{cuisines.join(", ")}</h4>
         <h4>{avgRating} stars</h4>
-        <h4>₹{costForTwo / 100} FOR TWO</h4>
+        <h4>{costForTwo}</h4>
       </div>
     );
   };
